9f710b1cb0ad873e0c40bd9f1af41da7aa688f20
Raquel-95 <fannypastor195@gmail.com>
2021-02-13 17:22:36 -0500

    crea los episodios (numeros) en la etiqueda de html desde JS}

--- src/main.js
+++ src/main.js
@@ -10,31 +10,18 @@
 
 import data from './data/rickandmorty/rickandmorty.js';
 
-// import {
-//   searchCharacters,
-// } from './data.js';
+// estas funciones son de ejemplo
+// export const example = () => {
+//   return 'example';
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
 
 
-//declarando id  15
-// const insertRickCharacter =document.querySelector('cards');
-// const searchResults=document.querySelector('#searchResults');
-
-// declarando id's del sitio web 19
-// const text =document.querySelector('#searchByName'); 
-
-// 169
-// const arrSearchCaracters=searchCharacters(data,text.value);
-// if(arrSearchCaracters.length>0){
-//   searchResults.innerHtml=cards(arrSearchCaracters);
-// }
-// else{
-//   searchResults.innerHTML='Nada de nada '
-// }
-
-
-
-function crear_episodios (){
-  let episodios = document.getElementById("episodios");
+function createEpisodes (){
+  let episodios = document.getElementById("episodes");
   for (let i = 1; i<=31; i++) {
     let option = document.createElement("option");//1° crear elemento de tipo option.
     let textnode = document.createTextNode(i);// 2° crear elemento de tipo texto.
@@ -42,59 +29,5 @@ function crear_episodios (){
     episodios.appendChild(option); //4° agrega elemento option dentro de elemento select. 
   }
 }
-crear_episodios();
+createEpisodes();
 
-
-function loadAll (){ //función para cargar/mostrar las tarjetas.
- let cards = document.getElementById("cards"); // vincula el elemento con id "cards" que es un article a la variable cards.
- cards.innerHTML=''; // para poder borrar el contenido de cards.
-
- data.results.forEach(function(valor, 
-  // indice, array
-  ){ //forEach sirve para recorrer los elementos de la lista results que está dentro de data. Dentro de forEach debe haber una función con parámetros valor, indice y array.
-  
-   let card = document.createElement("figure"); //1° crear un elemento en html de tipo figure.
-   
-   let imageCard = document.createElement("img");//2° crear un elemento en html de tipo img.
-   let nameCard = document.createElement("figcaption"); //3°crear un elemento en html de tipo figcaption.
- 
-   let textnode = document.createTextNode(valor.name); //4°crear nodo de texto con el contenido del nombre del personaje.
-   imageCard.src = valor.image; //5° cambia la propiedad src de elemento de imagen "img".
-
-   nameCard.appendChild(textnode);//añade la variable textnode que contiene el texto con el nombre a la variable nameCard.
-   card.appendChild(nameCard);//añade la variable nameCard que contiene figcaption a la variable card. 
-   card.appendChild(imageCard);//añade la variable imageCard a la variable card.
-   cards.appendChild(card);//añade la variable card a la variable cards que contiene todas las otras variables.
-  
-    card.addEventListener('click', () => {
-      showModal(valor);
-
-    });
-  });
-}
-loadAll(); //para llamar/ejecute a la función loadAll 
-
-function showModal (item) {
-  window.open("#myModal","_self");
-  //console.log(item);
-
-  let characterImage = document.getElementById('photoModal');
-  characterImage.src = item.image;
-  let characterStatus = document.getElementById('statusModal');
-  characterStatus.innerHTML = "Status: " + item.status;
-  let characterSpecies= document.getElementById('speciesModal');
-  characterSpecies.innerHTML = "Specie: " + item.species;
-  let characterType= document.getElementById('typeModal');
-  characterType.innerHTML = "Type: " + item.type;
-  let characterGender= document.getElementById('genderModal');
-  characterGender.innerHTML = "Gender: " + item.gender;
-  let characterOrigin= document.getElementById('originModal');
-  characterOrigin.innerHTML = "Origin: " + item.origin.name;
-  let characterLocation= document.getElementById('locationModal');
-  characterLocation.innerHTML = "Location: " + item.location.name;
-  //let characterEpisode= document.getElementById('episodeModal');
-  //characterEpisode.innerHTML = "Episode: " + item.episode;
-  let characterName = document.getElementById('nameModal');
-  characterName.innerHTML = "Name: " + item.name; // innetHTML se usa para cambiar el valor de un p.
-
-}

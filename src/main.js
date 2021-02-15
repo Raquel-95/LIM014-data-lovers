9847c08c0b470e2b65e5b2e50378202912223db0
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-02-15 09:19:47 -0500

    realizando cambios sobre importar la data en el main.js

--- src/main.js
+++ src/main.js
@@ -1,44 +1,29 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-// console.log(example());
+// import {allCharacters,order} from './data.js'
+import {allCharacters} from './data.js'
 
 
-// import { everithingData } from './data.js';
 
 import data from './data/rickandmorty/rickandmorty.js';
 
-// estas funciones son de ejemplo
-// export const example = () => {
-//   return 'example';
-// };
-
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
-
-
-function createEpisodes (){
-  let episodios = document.getElementById("episodes");
-  for (let i = 1; i<=31; i++) {
-    let option = document.createElement("option");//1° crear elemento de tipo option.
-    let textnode = document.createTextNode(i);// 2° crear elemento de tipo texto.
-    option.appendChild(textnode); //3° agrega el elemnto de texto dentro de elemento option.
-    episodios.appendChild(option); //4° agrega elemento option dentro de elemento select. 
-  }
-}
-createEpisodes();
+// function createEpisodes (){
+//   let episodios = document.getElementById("episodes");
+//   for (let i = 1; i<=31; i++) {
+//     let option = document.createElement("option");//1° crear elemento de tipo option.
+//     let textnode = document.createTextNode(i);// 2° crear elemento de tipo texto.
+//     option.appendChild(textnode); //3° agrega el elemnto de texto dentro de elemento option.
+//     episodios.appendChild(option); //4° agrega elemento option dentro de elemento select. 
+//   }
+// }
+// createEpisodes();
 
 function loadAll (){ //función para cargar/mostrar las tarjetas.
   let cards = document.getElementById("cards"); // vincula el elemento con id "cards" que es un article a la variable cards.
   cards.innerHTML=''; // para poder borrar el contenido de cards.
  
-  data.results.forEach(function(valor, 
-   // indice, array
-   ){ //forEach sirve para recorrer los elementos de la lista results que está dentro de data. Dentro de forEach debe haber una función con parámetros valor, indice y array.
-   
+  data.results.forEach(function(valor,indice,data){ //forEach sirve para recorrer los elementos de la lista results que está dentro de data. Dentro de forEach debe haber una función con parámetros valor, indice y array.
+   console.log(valor);
+
     let card = document.createElement("figure"); //1° crear un elemento en html de tipo figure.
     
     let imageCard = document.createElement("img");//2° crear un elemento en html de tipo img.
@@ -85,3 +70,53 @@ function loadAll (){ //función para cargar/mostrar las tarjetas.
 
 }
 
+// ***********************************************************click start home**************1
+document.querySelector('#btnFirst').addEventListener('click', getEnter );
+function getEnter(){
+    // *************************************************hide start home ******
+    let displayHome = document.getElementById("homeSection");
+    displayHome.classList.add("hide");
+    // ************************************************show header section******
+    let displayLogo=document.getElementById("logoHeader");
+    displayLogo.classList.remove("hide");
+    // *********************************************show principal nav section******
+    let displayPrincipalNav=document.getElementById("nav");
+    displayPrincipalNav.classList.remove("hide");
+    // **********************************************show search & order section******
+    let displaySubheader=document.getElementById("subheaderSection");
+    displaySubheader.classList.remove("hide");
+    // ************************************************show character section******
+    let displayCharacters=document.getElementById("charactersSection");
+    displayCharacters.classList.remove("hide");
+    // ************************************************show footer section******
+    let displayFooter=document.getElementById("footerSection");
+    displayFooter.classList.remove("hide");
+}
+// *******************************************************************************************click and return************2
+document.querySelector('#boxName').addEventListener('click', previousMain );
+ 
+function previousMain(){
+  // *************************************************show start home ******
+  let displayHome = document.getElementById("homeSection");
+  displayHome.classList.remove("hide");
+  // ************************************************remove header section******
+  let displayLogo=document.getElementById("logoHeader");
+  displayLogo.classList.add("hide");
+  // *********************************************remove principal nav section******
+  let displayPrincipalNav=document.getElementById("nav");
+  displayPrincipalNav.classList.add("hide");
+  // **********************************************remove search & order section******
+  let displaySubheader=document.getElementById("subheaderSection");
+  displaySubheader.classList.add("hide");
+  // ************************************************remove character section******
+  let displayCharacters=document.getElementById("charactersSection");
+  displayCharacters.classList.add("hide");
+  // ************************************************remove oothers about and stat******
+  let displayAbout=document.getElementById("aboutSection");
+  displayAbout.classList.add("hide");
+  let displayStatistics=document.getElementById("statisticsSection");
+  displayStatistics.classList.add("hide");
+  // ************************************************remove footer section******
+  let displayFooter=document.getElementById("footerSection");
+  displayFooter.classList.add("hide");
+}

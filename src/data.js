790a622a8f228aa134b06cdf925be37d68058667
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-02-16 10:17:28 -0500

    pull

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
 
 // order from a to z and z to a 
-export const order = (array, string1) => {
-    const arrayData = [...array];
-    const newArray = arrayData.map((obj) => obj.name)
-      .sort().map((string2) => arrayData.find((obj) => obj.name === string2));
-    if (string1 === 'A-Z') {
-      return newArray;
-    } if (string1 === 'Z-A') {
-      return newArray.reverse();
-    } 
-  };
+// export const order = (array, string1) => {
+//     const arrayData = [...array];
+//     const newArray = arrayData.map((obj) => obj.name)
+//       .sort().map((string2) => arrayData.find((obj) => obj.name === string2));
+//     if (string1 === 'A-Z') {
+//       return newArray;
+//     } if (string1 === 'Z-A') {
+//       return newArray.reverse();
+//     } 
+//   };
 
 
 

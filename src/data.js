90e059885920f50ab308cd57eeb5ab29b540bddd
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-02-25 10:06:58 -0500

    error

--- src/data.js
+++ src/data.js
@@ -80,39 +80,85 @@ export const dataSort = (allCharacters, sortBy, sortOrder) => {
 
 };
 
-/*export const kindFeaturesFilter = (allCharacters, statusAll, filterName, arrayFiltered) => {
-  console.log("entreeee");
-  let result = [];
-  console.log(statusAll);
-  result = allCharacters;
 
-  arrayFiltered = result;
-  console.log(result);
+// export const kindFeaturesFilter = (allCharacters, statusAll,filterName,arrayFiltered) => {
+//   console.log("entreeee");
+//     let result = [];
+//     console.log(statusAll);
+//     result = allCharacters;
+   
+//     arrayFiltered=result;
+//     console.log(result);
+   
+
+//     if(filterName == "origin"){
+//       console.log("entre a origin");
+//       // console.log(allCharacters[0][filterName].name);
+//       // console.log(statusAll);
+//       result = result.filter( character=> character[filterName].name == statusAll);
+
+//       // array.forEach(element => {
+        
+//       // });
+//       // result = allCharacters[0][filterName].name;
+//     }
+//  else if (filterName == "episodes"){
+//   console.log(epiii);
+//   result.forEach(element => {
+//     console.log(element[filterName]);
+//   });
+
+// }
+//     else{
+//       result = result.filter( character=> character[filterName] === statusAll); console.log(result);
+//     }
 
 
-  switch (filterName) {
-    case "origin":
-      console.log("entre a origin");
-      // console.log(allCharacters[0][filterName].name);
-      // console.log(statusAll);
-      result = result.filter(character => character[filterName].name == statusAll);
-      break;
+//     // if(filterName === "episodes"){
+     
+//     // }
+//  return result
+// };
 
-    case "episodes":
-      console.log("entre a epi");
-      // console.log(allCharacters[0][filterName].name);
-      // console.log(statusAll);
-      result.forEach(element => {
-        console.log(element[filterName]);
-      });
-      break;
 
-    default:
-      result = result.filter(character => character[filterName] === statusAll); console.log(result);
-      break;
-  }
-  // if(filterName === "episodes"){
 
-  // }
-  return result
-};*/
+export const kindFeaturesFilter = (allCharacters, statusAll,filterName,arrayFiltered) => {
+  // console.log("entreeee");
+    let result = [];
+    console.log(statusAll);
+    result = allCharacters;
+   
+    arrayFiltered=result;
+    // console.log(result);
+   
+
+
+
+    
+    switch (filterName) {
+      case "origin":
+        console.log("entre a origin");
+        // console.log(allCharacters[0][filterName].name);
+        // console.log(statusAll);
+        result = result.filter( character=> character[filterName].name == statusAll);
+        break;
+    
+        case "episodes":
+          // console.log("entre a epi");
+          // console.log(allCharacters[0][filterName].name);
+          // console.log(statusAll);
+
+          result.forEach(element => {
+            console.log(element[filterName]);
+          });
+          break;
+          
+      default:
+        result = result.filter( character=> character[filterName] === statusAll); console.log(result);
+        break;
+    }
+    // if(filterName === "episodes"){
+     
+    // }
+ return result
+};
